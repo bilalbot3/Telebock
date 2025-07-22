11f79024949da992906463adf333c75f93d7bb0f
bilalbot3 <bilalbbh19@gmail.com>
2025-07-22 15:59:16 +0100

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profilePicture: { type: String },
+  bio: { type: String },
+  telegramChatId: { type: Number },
+  telegramConnected: { type: Boolean, default: false },
+  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', UserSchema);
